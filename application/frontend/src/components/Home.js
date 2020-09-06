1d50dac0300a9e2856efc86c4c24e90dd6429315
Jeff Cheng <apisorder@gmail.com>
2020-09-06 10:11:00 -0700

    React router skeleton with buttons which point to blank member pages

---
+++ application/frontend/src/components/Home.js
@@ -0,0 +1,72 @@
+
+
+import React, { Component } from 'react';
+import apisorder from './apisorder';
+
+import {
+  BrowserRouter,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+export default class Home extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+
+          <ol>
+            <li>
+              <Link to='/allensun7'>
+                <button>Allen Sun</button>
+              </Link>
+            </li>
+
+            <li>
+              <Link to='/apisorder'>
+                <button>Jeff Cheng</button>
+              </Link>
+            </li>
+
+            <li>
+              <Link to='/Kebit01'>
+                <button>Kevin Ortiz</button>
+              </Link>
+            </li>
+
+            <li>
+              <Link to='/KevinWeiHadExtra'>
+                <button>Kevin Wei</button>
+              </Link>
+            </li>
+
+            <li>
+              <Link to='/npng16'>
+                <button>Nicole Pang</button>
+              </Link>
+            </li>
+
+            <li>
+              <Link to='/AsuPaul19'>
+                <button>Paul Asu</button>
+              </Link>
+            </li>
+          </ol>
+
+          <hr />
+
+          <Route path='/allensun7' />
+          <Route path='/apisorder' component={ apisorder } />
+          <Route path='/Kebit01' />
+          <Route path='/KevinWeiHadExtra' />
+          <Route path='/npng16' />
+          <Route path='/AsuPaul19' />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
---
+++ application/frontend/src/components/apisorder.js
@@ -0,0 +1,25 @@
+
+
+import React from 'react';
+
+export default function apisorder () {
+  return <div>
+          <h5>
+            <p>
+              Jeff is a second-year graduate student in Computer Science at San Francisco State University.
+                    
+              <br />
+              He graduated with a Bachelor of Arts in Economics, with a certified minor in Computer Science, from the University of California, Davis.
+
+                    <br />
+                    <br />
+                    He is interested in enhancing user-experience, preferrably
+                    for those with special needs.
+
+                    <br />
+                    <br />
+                    Jeff enjoys drawing, reading, and hiking in his spare time.
+                </p>
+          </h5>
+      </div>
+}
