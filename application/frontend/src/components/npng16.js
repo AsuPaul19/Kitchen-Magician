22afad66a4cd733260deb3540d37ee9870e27a85
Nicole Pang <43225858+npng16@users.noreply.github.com>
2020-09-09 20:21:43 -0700

    Add files via upload

---
+++ application/frontend/src/components/npng16.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const npng16 = () => {
+  return (
+    <div>
+      <h5>
+        <p>
+          Nicole Pang is a fourth year undergraduate computer science student at San Francisco State University.
+          <br />
+          <br />
+          In her free time she enjoys walking her three dogs, watching Netflix, and playing video games.
+        </p>
+      </h5>
+    </div>
+  );
+};
+
+export default npng16;
