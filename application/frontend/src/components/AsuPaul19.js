c74c83af8d3dc7c17ad57c121efb87835611628b
AsuPaul19 <pasu@mail.sfsu.edu>
2020-09-12 04:41:12 -0700

    added AsuPaul19.js and AsuPaul19Avatar.jpg file

---
+++ application/frontend/src/components/AsuPaul19.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import AsuPaul19Avatar from '../images/AsuPaul19Avatar.jpg'
+
+const AsuPaul19 = () => {
+  return (
+    <div>
+      <center>
+        <br>
+        </br>
+        <img src={AsuPaul19Avatar} alt="This is Paul Asu/"></img>
+        <h1>Welcome to my profile page.</h1>
+        
+        <p>
+          Asu Paul is a senior in the Computer Science department 
+          at the San Francisco State University.
+          <br>
+          </br>
+          I love traveling and spending time with my family.
+          <br>
+          </br>
+          Originally I'm from Nigeria-the most populous black nation, in West Africa 
+          <br>
+          </br>
+        </p>
+      </center>
+    </div>
+  );
+};
+
+export default AsuPaul19;
