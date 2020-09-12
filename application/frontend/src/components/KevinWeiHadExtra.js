13cd96d5d81abd68401861f4f1a91e7997a1822c
Kevin Wei <KevinWeiHadExtra>
2020-09-11 20:37:00 -0700

    change

--- application/frontend/src/components/KevinWeiHadExtra.js
+++ application/frontend/src/components/KevinWeiHadExtra.js
@@ -2,13 +2,11 @@ import React from 'react';
 import kevinWeiAvatar from '../images/kevinWeiAvatar.jpg';
 const KevinWeiHadExtra = () => {
   return (
-    <center>
-      <div>
         <div>
           <br />
-          <div>
+          <center>
             <img src={kevinWeiAvatar} alt='logo' />
-          </div>
+          </center>
           <br />
           <h1>About Kevin</h1>
           Kevin is a fourth year undergraduate student at San Francisco State
@@ -20,9 +18,7 @@ const KevinWeiHadExtra = () => {
           He really likes playing Video Games and has a special attachment to
           Pokemon since childhood :D
           <br />
-        </div>
       </div>
-    </center>
   );
 };
 export default KevinWeiHadExtra;
