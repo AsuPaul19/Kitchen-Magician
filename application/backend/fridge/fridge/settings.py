d0d7ed1ce7c057b331cb3880fa7033dfcc11c697
AllenSun7 <sunlun7@yahoo.com>
2020-09-25 16:41:29 -0500

    static for django amin

--- application/backend/fridge/fridge/settings.py
+++ application/backend/fridge/fridge/settings.py
@@ -135,4 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+import os.path  
+
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
