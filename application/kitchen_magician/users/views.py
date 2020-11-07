8b54d0935ed002831842bf49e60c810e610c97f8
apisorder <43589466+apisorder@users.noreply.github.com>
2020-11-06 16:38:33 -0800

    added django form, crispy form, but cannot fix register link error

--- application/kitchen_magician/users/views.py
+++ application/kitchen_magician/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# users/views.py
+# Programmer: Jeff C Cheng
+# Last Modified: 11/6/2020
+# use Django built-in form for registration page
 
-# Create your views here.
+
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+  # create the form
+  form = UserCreationForm()
+
+  context = {
+    {'form': form}
+  }
+  return render(request, 'users/register.html', context)
