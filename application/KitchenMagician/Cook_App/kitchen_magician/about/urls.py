af7b63e907bf45666fe152536f771e05b765def7
AsuPaul19 <pasu@mail.sfsu.edu>
2020-11-17 21:05:59 -0800

    added forgot_password.html

--- application/KitchenMagician/Cook_App/kitchen_magician/about/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.about, name='about'),
-    path('<str:name>-<str:id>/', views.profile, name='about-profile'),
-]
--- application/KitchenMagician/Cook_App/kitchen_magician/groups/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def groups(request):
-    context = {
-        'title': 'GROUPS'
-    }
-    return render(request, 'groups/groups.html', context)
